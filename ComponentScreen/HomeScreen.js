577515eaa410779275b86ddd09804fc6ca9a6027
Nikhil Agarwal <nikhil.agarwal@NikhilA-L.local>
2021-02-19 11:05:25 +0530

    vector icons include

--- ComponentScreen/HomeScreen.js
+++ ComponentScreen/HomeScreen.js
@@ -1,23 +1,20 @@
-import React from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import React from 'react';
+import {Text, View, StyleSheet, Button} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
+export default function HomeScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button
+        title="Post Data"
+        onPress={() => {
+          navigation.navigate('Post');
+        }}
+      />
 
+      <Button title="Get Data" onPress={() => navigation.navigate('Get')} />
 
-
-export default function HomeScreen({ navigation }) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-
-        <Button
-        title= "Post Data"
-        onPress= {()=> {navigation.navigate('Post'); }}
-        />
-
-        <Button
-          title="Get Data"
-          onPress={() => navigation.navigate('Get')}
-        />
-
-      </View>
-    );
-  }
+      {/* <Icon name="home" /> */}
+    </View>
+  );
+}
